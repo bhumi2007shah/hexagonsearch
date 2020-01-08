05307c880b1f5f4603dcee95c55180dc2dff95a1
SumitBagul <sumit@litmusblox.io>
2020-01-08 11:04:53 +0530

    Convert resume to text for cv rating #334

--- server/src/main/java/io/litmusblox/server/repository/JcmProfileSharingDetailsRepository.java
+++ server/src/main/java/io/litmusblox/server/repository/JcmProfileSharingDetailsRepository.java
@@ -6,6 +6,7 @@ package io.litmusblox.server.repository;
 
 import io.litmusblox.server.model.JcmProfileSharingDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -26,4 +27,9 @@ public interface JcmProfileSharingDetailsRepository extends JpaRepository<JcmPro
 
     @Transactional
     void deleteByJobCandidateMappingId(Long jobCandidateMappingId);
+
+    @Transactional
+    @Query(value = "select count(details.id) from jcm_profile_sharing_details details, jcm_profile_sharing_master master\n" +
+            "where master.sender_id =:userId and master.id = details.profile_sharing_master_id;", nativeQuery = true)
+    Integer getProfileSharingCount(Long userId);
 }
