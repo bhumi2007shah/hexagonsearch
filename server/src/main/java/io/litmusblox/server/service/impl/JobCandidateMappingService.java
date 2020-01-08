78c3a5363b74d857285a4fac2cd39462604d113b
SumitBagul <sumit@litmusblox.io>
2020-01-08 17:54:59 +0530

    Resume link in fetchCandidate api #332

--- server/src/main/java/io/litmusblox/server/service/impl/JobCandidateMappingService.java
+++ server/src/main/java/io/litmusblox/server/service/impl/JobCandidateMappingService.java
@@ -832,8 +832,7 @@ public class JobCandidateMappingService implements IJobCandidateMappingService {
             candidateDetails.setCvLocation(cvLocation.toString());
             candidateDetails.setCandidateId(returnObj);
             returnObj.setCandidateDetails(candidateDetails);
-            candidateRepository.save(returnObj);
-            candidateRepository.flush();
+            objFromDb.setCandidate(returnObj);
         }
         returnObj.setScreeningQuestionResponses(new ArrayList<>(screeningQuestionsMap.values()));
 
