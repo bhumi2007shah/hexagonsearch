1771d1c19efc12ab0ed04d8dfb5de1665cf2be7b
SumitBagul <sumit@litmusblox.io>
2020-01-28 12:30:30 +0530

    Design & Implement: Company shortname #336

--- server/src/main/java/io/litmusblox/server/service/impl/CompanyService.java
+++ server/src/main/java/io/litmusblox/server/service/impl/CompanyService.java
@@ -619,9 +619,9 @@ public class CompanyService implements ICompanyService {
         log.info("inside isCompanyExistForShortName");
         long startTime = System.currentTimeMillis();
         Company company = companyRepository.findByShortNameIgnoreCase(shortName);
-        log.info("Find company by shortName in " + (System.currentTimeMillis() - startTime) + "ms.");
+        log.info("Find company by shortName in {}ms.", (System.currentTimeMillis() - startTime));
         if(null != company){
-            log.info("Company already exist, CompanyId : "+company.getId());
+            log.info("Company already exist, CompanyId : {}",company.getId());
             return true;
         }else{
             return false;
