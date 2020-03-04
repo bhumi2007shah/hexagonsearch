f4d22722979a1a865dc28c3f76d56b61fb25bacc
sameer khan <sameer@hexagonsearch.com>
2020-03-04 15:39:05 +0530

    Change error message text for blocked users logging in #445

--- server/src/main/java/io/litmusblox/server/service/impl/LbUserDetailsService.java
+++ server/src/main/java/io/litmusblox/server/service/impl/LbUserDetailsService.java
@@ -107,6 +107,11 @@ public class LbUserDetailsService implements UserDetailsService {
             throw new WebException("Litmusblox cannot log you in right now. Please contact Litmusblox for further details", HttpStatus.FORBIDDEN);
         }
 
+        if(!IConstant.UserStatus.Active.toString().equals(userDetails.getStatus())){
+            log.error("User is not active, email: {}", user.getEmail());
+            throw new WebException("Your account has been blocked. Please contact your admin to get access to Litmusblox.", HttpStatus.FORBIDDEN);
+        }
+
         authenticate(user.getEmail(), user.getPassword());
 
         final String token = jwtTokenUtil.generateToken(userDetails, userDetails.getId(), userDetails.getCompany().getId());
