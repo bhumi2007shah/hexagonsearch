215da278d3a125068d91f3355099e63a30fbcbc3
sameer khan <sameer@hexagonsearch.com>
2020-06-24 12:59:21 +0530

    Save question tag for each question coming from searchEngine #566

--- server/src/main/resources/migration.sql
+++ server/src/main/resources/migration.sql
@@ -2466,4 +2466,5 @@ add column answer_selection varrchar(5);
 alter table tech_screening_question
 add column scoring_type varchar(5),
 add column default_answers varchar(100)[],
-add column answer_selection varchar(5);
+add column answer_selection varchar(5),
+add column question_tag varchar (50);
