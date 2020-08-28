3948ab017228b43ebb092a0791b7cabb80b259fa
sameer khan <sameer@hexagonsearch.com>
2020-08-28 18:53:42 +0530

    Security issue: Hide sensitive information from jwtToken #617

---
+++ server/src/main/java/io/litmusblox/server/utils/StringEncryptorDecryptor.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright © Litmusblox 2019. All rights reserved.
+ */
+
+package io.litmusblox.server.utils;
+
+import org.apache.commons.codec.binary.Base64;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author : sameer
+ * Date : 28/08/20
+ * Time : 4:32 PM
+ * Class Name : StringEncryptorDecryptor
+ * Project Name : server
+ */
+
+@Component
+public class StringEncryptorDecryptor {
+
+    private static final String encriptionKey = "C0T87r6PiEaGZmHp";
+    private static final String initVector = "E7bv4mGUXnWx0PBF";
+
+    public static String encrypt(String value){
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes(StandardCharsets.UTF_8));
+            SecretKeySpec skeySpec = new SecretKeySpec(encriptionKey.getBytes(StandardCharsets.UTF_8), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            return Base64.encodeBase64String(encrypted);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String decrypt(String encrypted){
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes(StandardCharsets.UTF_8));
+            SecretKeySpec skeySpec = new SecretKeySpec(encriptionKey.getBytes(StandardCharsets.UTF_8), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+            byte[] original = cipher.doFinal(Base64.decodeBase64(encrypted));
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+}
