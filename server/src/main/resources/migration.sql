89278b3775f564722a9b792f173c08ef9ef29d4a
sameer khan <sameer@hexagonsearch.com>
2020-10-06 14:06:51 +0530

    Chatbot enhancement #635

--- server/src/main/resources/migration.sql
+++ server/src/main/resources/migration.sql
@@ -2610,3 +2610,11 @@ alter table job_candidate_mapping drop column CANDIDATE_CHATBOT_RESPONSE;
 alter table job_candidate_mapping add column CANDIDATE_CHATBOT_RESPONSE hstore;
 update job_candidate_mapping jcm set candidate_chatbot_response = cr.responseList from (select job_candidate_mapping_id, hstore(array_agg(case when comment is not null then string_to_array(concat(job_screening_question_id,'^*^',concat(response, '~', comment)), '^*^') else string_to_array(concat(job_screening_question_id,'^*^',response), '^*^') end order by id)) as responseList from candidate_screening_question_response group by 1) as cr where jcm.id = cr.job_candidate_mapping_id;
 --Run create view for export data
+
+
+-- For ticket #635
+update screening_question set options = options || '{I wish not to answer}' where question_type in (97, 98, 100);
+
+insert into master_data(type, value, value_to_use) values ('questionType', 'FutureCalendar', 'Future Calendar'),('questionType', 'PastCalendar', 'Past Calendar');
+
+update screening_question set question_type=(select id from master_data where value='FutureCalendar') where question_type=(select id from master_data where value='Calendar');
