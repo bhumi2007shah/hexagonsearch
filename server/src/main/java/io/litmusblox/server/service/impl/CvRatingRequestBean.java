5ee5f78a6f02a7a33e73e8225a2e508c30c9ecc4
sumit <sumit@litmusblox.io>
2020-10-23 09:06:43 +0530

    Store skills returned during CV parsing #666

--- server/src/main/java/io/litmusblox/server/service/impl/CvRatingRequestBean.java
+++ server/src/main/java/io/litmusblox/server/service/impl/CvRatingRequestBean.java
@@ -23,6 +23,6 @@ import java.util.Map;
 @AllArgsConstructor
 public class CvRatingRequestBean {
     Map<String, List<String>> jdKeySkillsMap;
-    List<String> resumeContent;
+    List<String> resumeContentSkills;
     String industry;
 }
