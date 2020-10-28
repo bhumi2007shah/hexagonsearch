e5c936a9d04b5de713642c449e2330cdce808d24
prashant <prashant.agrawal@litmusblox.io>
2020-10-28 14:06:34 +0530

    Hiring Manager interest information to frontend #679

--- server/src/main/resources/migration.sql
+++ server/src/main/resources/migration.sql
@@ -2726,3 +2726,4 @@ ADD COLUMN CV_RATING_API_CALL_RETRY_COUNT INTEGER DEFAULT 0;
 
 --For ticket #679
 update jcm_profile_sharing_master psm SET receiver_name = concat(u.first_name, ' ', u.last_name) from users u where psm.receiver_name is null and psm.receiver_id = u.id;
+alter table jcm_profile_sharing_master drop column receiver_email ;
