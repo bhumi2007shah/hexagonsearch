82c92ce6f4b2cc2b23d13f0d820f1824cb28eb40
AJAY <ajayagr787@gmail.com>
2021-03-08 15:13:50 +0530

    Candidate search #777.
    changes in code

--- server/src/main/java/io/litmusblox/server/repository/UserRepository.java
+++ server/src/main/java/io/litmusblox/server/repository/UserRepository.java
@@ -5,7 +5,11 @@
 package io.litmusblox.server.repository;
 
 import io.litmusblox.server.model.User;
+import io.litmusblox.server.service.UserWorkspaceBean;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.UUID;
@@ -19,8 +23,13 @@ import java.util.UUID;
  * Class Name : UserRepository
  * Project Name : server
  */
-public interface UserRepository extends JpaRepository<User, Long> {
-
+public interface UserRepository extends JpaRepository<User, Long>
+{
+    @Transactional
+    @Query("SELECT  new io.litmusblox.server.service.UserWorkspaceBean(u.id,CONCAT(u.firstName, ' ', u.lastName) AS userName,u.status,u.companyAddressId,u.companyBuId,u.email," +
+            "u.mobile,u.userType)" +
+            "FROM User u WHERE u.company.id=:validCompanyId")
+    List<UserWorkspaceBean> findWorkspaceData(Long validCompanyId);
     List<User> findByCompanyId(Long companyId);
     User findByEmail(String email);
     User findByUserUuid(UUID userUuid);
@@ -28,4 +37,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     int countByCompanyId(Long companyId);
     int countByCompanyBuId(Long companyBuId);
     List<User> findByIdIn(List<Long> userId);
+
+
 }
